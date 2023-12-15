6f630f62bbee5adf0c7011fcad4a212f3db342fe
a-dugonin <a_dugonin@mail.ru>
2023-12-15 14:02:44 +0300

    Задача #1: Добавление типов в методы

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from matrix import Matrix
 
-m1 = Matrix(count_rows=2, count_columns=3)
-m1.matrix = [[1, 2, 3], [4, 5, 6]]
-m2 = Matrix(count_rows=2, count_columns=3)
-m2.matrix = [[7, 8, 9], [10, 11, 12]]
-print(m1, '\n')
-print(m2, '\n')
-print(m1.add(m2), '\n')
-print(m1.subtract(m2), '\n')
-m3 = Matrix(count_rows=3, count_columns=2)
-m3.matrix = [[1, 2], [3, 4], [5, 6]]
-print(m3, '\n')
-print(m1.multiply(m3), '\n')
-print(m1.transpose())
+m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+m2 = Matrix([[7, 8, 9], [10, 11, 12]])
+m3 = Matrix([[1, 2], [3, 4], [5, 6]])
+print('Матрица №1', m1, sep='\n')
+print('Матрица №2', m2, sep='\n')
+print('Матрица №3', m3, sep='\n')
+print('Сложение матриц №1 и №2', m1.add(m2), sep='\n')
+print('Вычитание из матрицы №1, матрицы №2', m1.subtract(m2), sep='\n')
+print('Умножение матриц №1 и №3', m1.multiply(m3), sep='\n')
+print('Транспонирование матрицы №1', m1.transpose(), sep='\n')
--- matrix.py
+++ matrix.py
@@ -2,51 +2,41 @@ from numpy import array, add, subtract, matmul, transpose
 
 
 class Matrix:
-    def __init__(self, count_rows, count_columns, matrix: array = None):
-        self.count_rows = count_rows
-        self.count_columns = count_columns
+    """ Класс для создания экземпляров матриц """
+    def __init__(self, matrix: array = None):
         self.matrix = array(matrix)
 
-    def add(self, another_matrix):
+    def add(self, another_matrix: array = None) -> array:
         add_matrix = add(self.matrix, another_matrix.matrix)
-        return f'{'\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in add_matrix])}'
+        return '\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in add_matrix])
 
-    def subtract(self, another_matrix):
+    def subtract(self, another_matrix: array = None) -> array:
         subtract_matrix = subtract(self.matrix, another_matrix.matrix)
-        return f'{'\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in subtract_matrix])}'
+        return '\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in subtract_matrix])
 
-    def multiply(self, another_matrix):
-        dot_matrix = matmul(self.matrix, another_matrix.matrix)
-        return f'{'\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in dot_matrix])}'
+    def multiply(self, another_matrix: array = None) -> array:
+        multiply_matrix = matmul(self.matrix, another_matrix.matrix)
+        return '\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in multiply_matrix])
 
-    def transpose(self):
+    def transpose(self) -> array:
         transposed_matrix = transpose(self.matrix)
-        return f'{'\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in transposed_matrix])}'
+        return '\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in transposed_matrix])
 
     def __str__(self):
         return '\n'.join([' '.join([str(value).ljust(3) for value in row]) for row in self.matrix])
 
 
 def main():
-    m1 = Matrix(count_rows=2, count_columns=3)
-    m1.matrix = [[1, 2, 3], [4, 5, 6]]
-    m2 = Matrix(count_rows=2, count_columns=3)
-    m2.matrix = [[7, 8, 9], [10, 11, 12]]
-    print(m1)
-    print()
-    print(m2)
-    print()
-    print(m1.add(m2))
-    print()
-    print(m1.subtract(m2))
-    print()
-    m3 = Matrix(count_rows=3, count_columns=2)
-    m3.matrix = [[1, 2], [3, 4], [5, 6]]
-    print(m3)
-    print()
-    print(m1.multiply(m3))
-    print()
-    print(m1.transpose())
+    m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+    m2 = Matrix([[7, 8, 9], [10, 11, 12]])
+    print('Матрица №1', m1, sep='\n')
+    print('Матрица №2', m2, sep='\n')
+    print('Сложение матриц №1 и №2', m1.add(m2), sep='\n')
+    print('Вычитание из матрицы №1, матрицы №2', m1.subtract(m2), sep='\n')
+    m3 = Matrix([[1, 2], [3, 4], [5, 6]])
+    print('Матрица №3', m3, sep='\n')
+    print('Умножение матриц №1 и №3', m1.multiply(m3), sep='\n')
+    print('Транспонирование матрицы №1', m1.transpose(), sep='\n')
 
 
 if __name__ == '__main__':
